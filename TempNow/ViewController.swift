e9c2cebce2559264c4bee62e9bbf53b79dd0ff0f
chenwang <cwang19@uci.edu>
2018-11-28 23:07:10 -0800

    Initial Commit

---
+++ TempNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempNow
+//
+//  Created by Chen Wang on 11/28/18.
+//  Copyright © 2018 utopia incubator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
