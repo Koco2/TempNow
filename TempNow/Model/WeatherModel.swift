3b8b5c91cd484a242565a4bad16db1a19777532e
chenwang <cwang19@uci.edu>
2018-11-28 23:56:13 -0800

    finished!!!again

---
+++ TempNow/Model/WeatherModel.swift
@@ -0,0 +1,61 @@
+//
+//  WeatherModel.swift
+//  TempNow
+//
+//  Created by Chen Wang on 11/28/18.
+//  Copyright © 2018 utopia incubator. All rights reserved.
+//
+
+import Foundation
+
+class WeatherModel {
+    
+    //Declare your model variables here
+    var temperature : Int = 0
+    var condition : Int = 0
+    var city : String = ""
+    var weatherIconName : String = ""
+    
+    //This method turns a condition code into the name of the weather condition image
+    
+    func updateWeatherIcon(condition: Int) -> String {
+        
+        switch (condition) {
+            
+        case 0...300 :
+            return "storm1"
+            
+        case 301...600 :
+            return "rainy"
+            
+        case 601...700 :
+            return "snowy"
+            
+        case 701...771 :
+            return "fog"
+            
+        case 772...799 :
+            return "storm2"
+            
+        case 800 :
+            return "sunny"
+            
+        case 801...804 :
+            return "cloudy1"
+            
+        case 900...903, 905...1000  :
+            return "stom2"
+            
+        case 903 :
+            return "snowy"
+            
+        case 904 :
+            return "sunny"
+            
+        default :
+            return "dunno"
+        }
+        
+    }
+}
+
