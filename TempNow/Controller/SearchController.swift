4de5f92f1732a99e690d2c228c45ed528cee792e
chenwang <cwang19@uci.edu>
2018-12-01 22:42:51 -0800

    all UI have been completed

---
+++ TempNow/Controller/SearchController.swift
@@ -0,0 +1,106 @@
+//
+//  SearchController.swift
+//  TempNow
+//
+//  Created by Chen Wang on 11/30/18.
+//  Copyright © 2018 utopia incubator. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+    
+    var background:UIImageView!
+    var controllerSwitch:UIButton!
+    var getWeatherButton:UIButton!
+    var input:UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        addBackGroundImage()
+        addButtons()
+        addTextField()
+        addConstrains()
+
+    }
+    
+    private func addBackGroundImage(){
+        background = UIImageView(image: UIImage(named: "background2"))
+        background.contentMode = UIView.ContentMode.scaleToFill
+        self.view.addSubview(background)
+    }
+    
+    private func addButtons(){
+        controllerSwitch = UIButton()
+        controllerSwitch.setBackgroundImage(UIImage(named: "switch"), for: .normal)
+        controllerSwitch.addTarget(self, action: #selector(controllerSwitchPressed), for: .touchUpInside)
+        self.view.addSubview(controllerSwitch)
+        
+        getWeatherButton = UIButton()
+        getWeatherButton.setTitle("Get Weather", for: .normal)
+        getWeatherButton.titleLabel?.font = .systemFont(ofSize: 20)
+        getWeatherButton.addTarget(self, action: #selector(getWeatherButtonPressed), for: .touchUpInside)
+        self.view.addSubview(getWeatherButton)
+        
+    }
+    
+    @objc private func controllerSwitchPressed(){
+        self.dismiss(animated: true) {
+            if(self.input.text! != ""){
+                print(self.input.text!)
+            }else{
+                print("empty input")
+            }
+        }
+    }
+    
+    @objc private func getWeatherButtonPressed(){
+        controllerSwitchPressed()
+    }
+    
+    private func addTextField(){
+        input = UITextField()
+        input.borderStyle = UITextField.BorderStyle.roundedRect
+        self.view.addSubview(input)
+    }
+    
+    
+    private func addConstrains(){
+        background.snp.makeConstraints { (make) in
+            make.top.left.right.bottom.equalTo(0)
+        }
+        
+        controllerSwitch.snp.makeConstraints { (make) in
+            make.top.equalTo(20)
+            make.right.equalTo(-20)
+            make.height.width.equalTo(50)
+        }
+        
+        input.snp.makeConstraints { (make) in
+            make.top.equalTo(UIScreen.main.bounds.size.height/4.5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(UIScreen.main.bounds.size.width/1.8)
+            make.height.equalTo(30)
+        }
+        
+        getWeatherButton.snp.makeConstraints { (make) in
+            make.top.equalTo(input.snp.bottom).offset(10)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(UIScreen.main.bounds.size.width/1.8)
+            make.height.equalTo(30)
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TempNow/Controller/ViewController.swift
+++ TempNow/Controller/ViewController.swift
@@ -2,19 +2,156 @@
 //  ViewController.swift
 //  TempNow
 //
-//  Created by Chen Wang on 11/28/18.
+//  Created by Chen Wang on 11/30/18.
 //  Copyright © 2018 utopia incubator. All rights reserved.
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
+    
+    var background:UIImageView!
+    var contentView:UIView!
+    var controllerSwitch:UIButton!
+    var tempLabel:UILabel!
+    var weatherIcon:UIImageView!
+    var CtoF:UISwitch!
+    var cityLabel:UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        addBackGroundImage()
+        addButtons()
+        addSwitch()
+        createDisplayView()
+        addConstrains()
+        print()
+    }
+    
+    private func addBackGroundImage(){
+        background = UIImageView(image: UIImage(named: "background1"))
+        background.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.addSubview(background);
+    }
+    
+    private func addButtons(){
+        controllerSwitch = UIButton()
+        controllerSwitch.setBackgroundImage(UIImage(named: "switch"), for: .normal)
+        controllerSwitch.addTarget(self, action: #selector(controllerSwitchPressed), for: .touchUpInside)
+        self.view.addSubview(controllerSwitch)
+        
+    }
+    
+    @objc private func controllerSwitchPressed(){
+        print("controllerSwitchPressed")
+        self.present(SearchController(), animated: true, completion: nil)
+    }
+    
+    private func addSwitch(){
+        CtoF = UISwitch()
+        CtoF.addTarget(self, action: #selector(switchPressed(_:)), for: .valueChanged)
+        self.view.addSubview(CtoF)
+    }
+    
+    @objc private func switchPressed(_ sender:UISwitch){
+        print(sender.isOn)
+    }
+    
+    private func createDisplayView(){
+        contentView = UIView()
+        contentView.isUserInteractionEnabled = true
+        //contentView.backgroundColor = UIColor.orange
+        self.view.addSubview(contentView)
+        
+        tempLabel = UILabel()
+        tempLabel.text = "21°C"
+        //tempLabel.backgroundColor = UIColor.gray
+        tempLabel.textAlignment = NSTextAlignment.right
+        tempLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 100)
+        tempLabel.textColor = UIColor.white
+        
+        contentView.addSubview(tempLabel)
+        
+        weatherIcon = UIImageView()
+        //weatherIcon.backgroundColor = UIColor.green
+        weatherIcon.contentMode = .scaleAspectFit
+        weatherIcon.image = UIImage(named: "sunny")
+        contentView.addSubview(weatherIcon)
+        
+        cityLabel = UILabel()
+        cityLabel.text = "unknown city"
+        cityLabel.textColor = UIColor.white
+        //cityLabel.backgroundColor = UIColor.purple
+        cityLabel.font = UIFont(name: "HelveticaNeue-Medium", size: 30)
+        contentView.addSubview(cityLabel)
+        
+    }
+    
+    private func addConstrains(){
+        background.snp.makeConstraints { (make) in
+            make.top.left.right.bottom.equalTo(0)
+        }
+        controllerSwitch.snp.makeConstraints { (make) in
+            make.top.equalTo(30)
+            make.right.equalTo(-20)
+            make.height.width.equalTo(50)
+        }
+        
+        CtoF.snp.makeConstraints { (make) in
+            make.centerY.equalTo(controllerSwitch.snp.centerY)
+            make.left.equalTo(25)
+ 
+        }
+        
+        contentView.snp.makeConstraints { (make) in
+            make.top.equalTo(UIScreen.main.bounds.size.height/3-40)
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+            make.bottom.equalTo(-40)
+        }
+        
+        tempLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(contentView.snp.top)
+            make.left.equalTo(contentView.snp.left)
+            make.right.equalTo(contentView.snp.right)
+            make.height.equalTo(UIScreen.main.bounds.size.height*(2/3)/3)
+            
+        }
+        
+        weatherIcon.snp.makeConstraints { (make) in
+            make.top.equalTo(tempLabel.snp.bottom)
+            make.left.equalTo(contentView.snp.left)
+            make.right.equalTo(contentView.snp.right)
+            make.height.equalTo(UIScreen.main.bounds.size.height*(2/3)*(2/3)-70)
+            
+        }
+        
+        cityLabel.snp.makeConstraints { (make) in
+            
+            make.top.equalTo(weatherIcon.snp.bottom)
+            make.left.equalTo(contentView.snp.left)
+            make.right.equalTo(contentView.snp.right)
+            make.bottom.equalTo(0)
+
+            
+        }
+        
     }
 
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
